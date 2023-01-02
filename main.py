dcbef10d42b34e664fb7158302b4e8374b07e1e8
Hiroyuki-Ishige <hiroyukiishige@hotmail.com>
2023-01-03 07:58:49 +1100

    Change setting at SECRET_KEY and DATABASE_URL (5)

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 #Set database after HEROKU deployment
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://zahxfbxoaztmvs:ff36dcf61ca45eee12247ee2a4093bdc4cf3990aaeaced3f2e5610c36e93e7ca@ec2-34-197-84-74.compute-1.amazonaws.com:5432/d6p4ujefhed2pe"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://zahxfbxoaztmvs:ff36dcf61ca45eee12247ee2a4093bdc4cf3990aaeaced3f2e5610c36e93e7ca@ec2-34-197-84-74.compute-1.amazonaws.com:5432/d6p4ujefhed2pe"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
